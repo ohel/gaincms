88dba474cdb855716984d50d01589857827d3d08
ohel <ohel@users.noreply.github.com>
2016-11-10 18:47:50 +0200

    Fix typo.

--- site/post.php
+++ site/post.php
@@ -62,7 +62,7 @@ include(DIR_INCLUDE . "/comments.php");
 
 <div class="to-top-button-container"><a onclick="scrollToTop()" title="To top">▲</a></div>
 
-<footer><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a>&nbsp;This article by <?php echo CONFIG_AUTHOR?> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a><footer>
+<footer><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a>&nbsp;This article by <?php echo CONFIG_AUTHOR?> is licensed under the <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a><footer>
 
 <?php include(DIR_INCLUDE . "/footer.php")?>
 
