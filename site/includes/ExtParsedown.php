84c9d3f683e115844787a0e6a3dccaa1b96f675f
ohel <ohel@users.noreply.github.com>
2020-08-31 02:32:34 +0300

    Fix: create valid HTML.

--- site/includes/ExtParsedown.php
+++ site/includes/ExtParsedown.php
@@ -19,14 +19,36 @@ class ExtParsedown extends Parsedown
         return $this;
     }
 
+    # Fixes non-phrasing elements inside paragraphs, inserted by the overridden element method below.
+    # Browsers do fix these anyway on the fly, but the end result isn't clean.
+    function text($text) {
+
+        $html = parent::text($text);
+
+        # Fixes <p><a><div>... into <p>...</p><div><a>... (the order of <a> and <div> changes)
+        # The regex is known as a tempered greedy token, using a negative lookahead.
+        $html = preg_replace(
+            "/(<p>(?:(?!<\/p>).)*)(<a.*>)(<div class=\"img-container\">)(.*)<\/div><\/a>(.*)<\/p>/",
+            '$1</p>$3$2$4</a></div><p>$5</p>', $html);
+
+        # Fixes <p><div>... into <p>...</p><div>...
+        $html = preg_replace(
+            "/(<p>(?:(?!<\/p>).)*)(<div class=\"img-container\">.*<\/div>)(.*)<\/p>/",
+            '$1</p>$2<p>$3</p>', $html);
+
+        # Removes empty paragraphs possibly created by the two fixes above.
+        return str_replace('<p></p>', '', $html);
+    }
+
     # Override for img elements with title: put them inside figure elements and give them a caption.
+    # This needs the text method to fix the resulting HTML:
+    # non-phrasing elements (div, figure) are not allowed inside paragraphs.
     protected function element(array $Element) {
 
         if ($Element['name'] != 'img') {
             return parent::element($Element);
         }
 
-        // TODO/FIXME: cannot have div or figure elements inside p elements
         $markup = '<div class="img-container">';
         if (isset($Element['attributes']['title'])) {
             $markup .= '<figure>';
